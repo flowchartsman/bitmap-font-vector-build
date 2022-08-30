96b0a7da4e8afe9bb85aa08e8083237d1b562773
Andy Walker <walkeraj@gmail.com>
2022-08-30 13:08:51 -0400

    build crossplatform images

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-docker build . -t flowchartsman/bitmap-font-vector-build:latest
+if docker buildx ls | grep -qv 'multiplatform'; then
+    docker buildx create --name multiplatform
+fi
+docker buildx use multiplatform
+docker buildx inspect --bootstrap
+docker buildx build --platform linux/amd64,linux/arm64/v8 -t flowchartsman/bitmap-font-vector-build:latest .
